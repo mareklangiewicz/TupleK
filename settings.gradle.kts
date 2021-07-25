fcf9b1b0a0606bd8069e286256c6218ea081055c
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-07-25 20:45:07 +0200

    experiments

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
 @file:Suppress("UnstableApiUsage")
+@file:OptIn(okio.ExperimentalFileSystem::class)
+
+import okio.Path.Companion.toPath
+
+//gradle.logSomeEventsToFile("my.gradle.log".toPath())
 
 pluginManagement {
     includeBuild("../deps.kt")
