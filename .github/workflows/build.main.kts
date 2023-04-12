d17966861e9e7953461f6c76830f201138f66925
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-04-12 21:57:52 +0200

    fun

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -1,14 +1,30 @@
 #!/usr/bin/env kotlin
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:0.40.1")
-@file:Import("_shared.main.kts")
 
 import io.github.typesafegithub.workflows.actions.actions.CheckoutV3
+import io.github.typesafegithub.workflows.actions.actions.SetupJavaV3
 import io.github.typesafegithub.workflows.actions.gradle.GradleBuildActionV2
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.triggers.PullRequest
 import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.JobBuilder
+import io.github.typesafegithub.workflows.dsl.expressions.expr
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.writeToFile
+
+// Warning: Might need to clean cache after changing file
+//   ~/.cache$ rm -rf main.kts.compiled.cache/
+
+fun JobBuilder<*>.setupJava() =
+    uses(
+        name = "Set up JDK",
+        action = SetupJavaV3(
+            javaVersion = "17",
+            distribution = SetupJavaV3.Distribution.Zulu,
+        )
+    )
+
+val notMyFork = expr { "${github.repository_owner} != 'langara'" }
 
 workflow(
     name = "Build",
