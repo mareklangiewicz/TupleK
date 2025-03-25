9e61ab642f307c69a42509bbf9e5d56da3eb9554
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-03-25 14:09:41 +0100

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import pl.mareklangiewicz.deps.*
 import pl.mareklangiewicz.utils.*
 
 plugins {
-  plug(plugs.NexusPublish)
   plug(plugs.KotlinMulti) apply false
 }
 
@@ -15,7 +14,7 @@ defaultBuildTemplateForRootProject(
     name = "TupleK",
     description = "Tiny tuples lib for Kotlin with cool infix syntax.",
     githubUrl = "https://github.com/mareklangiewicz/TupleK",
-    version = Ver(0, 0, 19),
+    version = Ver(0, 0, 20),
     // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/tuplek/
     // https://github.com/mareklangiewicz/TupleK/releases
     settings = LibSettings(
@@ -29,49 +28,10 @@ defaultBuildTemplateForRootProject(
 
 // region [[Root Build Template]]
 
-/** Publishing to Sonatype OSSRH has to be explicitly allowed here, by setting withSonatypeOssPublishing to true. */
 fun Project.defaultBuildTemplateForRootProject(details: LibDetails? = null) {
-  ext.addDefaultStuffFromSystemEnvs()
   details?.let {
     rootExtLibDetails = it
     defaultGroupAndVerAndDescription(it)
-    if (it.settings.withSonatypeOssPublishing) defaultSonatypeOssNexusPublishing()
-  }
-
-  // kinda workaround for kinda issue with kotlin native
-  // https://youtrack.jetbrains.com/issue/KT-48410/Sync-failed.-Could-not-determine-the-dependencies-of-task-commonizeNativeDistribution.#focus=Comments-27-5144160.0-0
-  repositories { mavenCentral() }
-}
-
-/**
- * System.getenv() should contain six env variables with given prefix, like:
- * * MYKOTLIBS_signing_keyId
- * * MYKOTLIBS_signing_password
- * * MYKOTLIBS_signing_keyFile (or MYKOTLIBS_signing_key with whole signing key)
- * * MYKOTLIBS_ossrhUsername
- * * MYKOTLIBS_ossrhPassword
- * * MYKOTLIBS_sonatypeStagingProfileId
- * * First three of these used in fun pl.mareklangiewicz.defaults.defaultSigning
- * * See KGround/template-full/template-full-lib/build.gradle.kts
- */
-fun ExtraPropertiesExtension.addDefaultStuffFromSystemEnvs(envKeyMatchPrefix: String = "MYKOTLIBS_") =
-  addAllFromSystemEnvs(envKeyMatchPrefix)
-
-fun Project.defaultSonatypeOssNexusPublishing(
-  sonatypeStagingProfileId: String = rootExtString["sonatypeStagingProfileId"],
-  ossrhUsername: String = rootExtString["ossrhUsername"],
-  ossrhPassword: String = rootExtString["ossrhPassword"],
-) {
-  nexusPublishing {
-    this.repositories {
-      sonatype {  // only for users registered in Sonatype after 24 Feb 2021
-        stagingProfileId put sonatypeStagingProfileId
-        username put ossrhUsername
-        password put ossrhPassword
-        nexusUrl put repos.sonatypeOssNexus
-        snapshotRepositoryUrl put repos.sonatypeOssSnapshots
-      }
-    }
   }
 }
 
