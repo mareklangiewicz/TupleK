c0a43ae4964b63d55982a8b6ed7cb65edab58021
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-07-04 16:29:46 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ defaultBuildTemplateForRootProject(
     name = "TupleK",
     description = "Tiny tuples lib for Kotlin with cool infix syntax.",
     githubUrl = "https://github.com/mareklangiewicz/TupleK",
-    version = Ver(0, 0, 20),
+    version = Ver(0, 0, 21),
     // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/tuplek/
     // https://github.com/mareklangiewicz/TupleK/releases
     settings = LibSettings(
